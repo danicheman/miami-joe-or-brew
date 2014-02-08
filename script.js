7800a036845ad841faf7c737a41b119484d9a401
Marcos Savoury <marcossavoury@hotmail.com>
2014-02-07 22:14:14 -0500

    more js

--- script.js
+++ script.js
@@ -8,7 +8,16 @@ window.game = (window.game || {});
 
 game.test = function() {
 	alert('js is working');
-	//
+}
+
+game.sendAnswer = function() {
+	$.post( "/api.php","string", function( data ) {
+		alert(data);
+	});
+}
+
+game.questionReceived = function (question) {
+
 }
 
 $(document).ready(function() {
