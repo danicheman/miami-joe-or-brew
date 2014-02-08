c7bc03b31d09baf412436e928df2d7c3210b31eb
Marcos Savoury <marcossavoury@hotmail.com>
2014-02-07 22:02:11 -0500

    more script changes

--- script.js
+++ script.js
@@ -7,12 +7,28 @@
 window.game = (window.game || {});
 
 game.test = function() {
-	alert('js is working');
-	//
+
+}
+
+game.sendAnswer = function() {
+
+	$.post( "/api.php","string_data", function( data ) {
+			})
+
+}
+
+game.updateGameUI = function () {
+
+}
+
+game.answerClicked = function () {
+
 }
 
 $(document).ready(
-
-	game.test();
+		function() {
+			game.test();
+			//bind the answer functions to the buttons
+		}
 
 );
