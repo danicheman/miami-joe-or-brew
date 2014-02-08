968fa45186bab70a43cbd1bab436347a0c4b104c
Marcos Savoury <marcossavoury@hotmail.com>
2014-02-08 00:10:24 -0500

    test

--- script.js
+++ script.js
@@ -10,6 +10,8 @@ game.test = function() {
 	alert('js is working');
 }
 
+game.question_index = 0;
+
 game.randomQuestionHint = function () {
 	var hints = [
 		"Is it a colada or a chelada?",
@@ -42,17 +44,18 @@ game.startGame = function() {
 
 game.answerClicked = function(e) {
 	e.preventDefault();
-	var answer = $(this).attr('id');
+	var answer = $(this).parents('section').attr('id');
 	var answer_data = {};
 	answer_data.answer = answer;
 	answer_data.type = "answer";
+	answer_data.question_index = game.question_index;
 
 	game.sendAnswer(answer_data);
 }
 
 game.questionReceived = function (question) {
 	var question_text = question.q;
-	var question_index = question.index;
+	game.question_index = question.question_index;
 	$('#question_text').html(question_text);
 	$('#question_hint').html(game.randomQuestionHint());
 }
