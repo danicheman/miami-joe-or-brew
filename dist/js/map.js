cbfafe2426b6bb43252e88f2ab282fe1382291f3
Nick O <danicheman@yahoo.com>
2014-02-08 02:26:13 -0500

    asdf

--- dist/js/map.js
+++ dist/js/map.js
@@ -1,3 +1,5 @@
+var map;
+
 function initialize(lat,lng) {
   var myLatlng = new google.maps.LatLng(lat,lng);
   var mapOptions = {
@@ -6,7 +8,7 @@ function initialize(lat,lng) {
   };
   
   
-  var map = new google.maps.Map(document.getElementById('map'),
+  map = new google.maps.Map(document.getElementById('map'),
       mapOptions);
   
    var marker = new google.maps.Marker({
@@ -16,9 +18,21 @@ function initialize(lat,lng) {
   });   
 }
 
+function movePoint(hopLat, hopLng) {
+    var pnt = new GLatLng(hopLat, hopLng);
+    map.panTo(pnt);
+}
+
 setTimeout(function() {
     myLat = 25;
     myLong = -80;
     console.log("timeout");
     initialize(myLat,myLong);
 },1000);
+
+setTimeout(function() {
+    myLat = 23;
+    myLong = -79;
+    console.log("timeout");
+    movePoint(myLat,myLong);
+},2000);
