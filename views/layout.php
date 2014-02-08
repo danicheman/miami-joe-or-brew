8163688ff8ecf2982308c0306ca0b101451a0858
Nick O <danicheman@yahoo.com>
2014-02-07 21:21:41 -0500

    asdfasfd

--- views/layout.php
+++ views/layout.php
@@ -18,7 +18,7 @@
     <link href="jumbotron-narrow.css" rel="stylesheet">
 
     <!-- Just for debugging purposes. Don't actually copy this line! -->
-    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <!- -[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
 
     <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
@@ -45,7 +45,12 @@
         <p><a class="btn btn-lg btn-success" href="#" role="button">Sign up today</a></p>
       </div>
 
-      <div class="row marketing">
+      <?php
+        if(in_array($_GET['page'],['start','win', 'lose','game'])) {
+            include($_GET['page'].'.php');
+        }
+      ?>
+      <!--<div class="row marketing">
         <div class="col-lg-6">
           <h4>Subheading</h4>
           <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
@@ -67,7 +72,7 @@
           <h4>Subheading</h4>
           <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
         </div>
-      </div>
+      </div>-->
 
       <div class="footer">
         <p>&copy; Company 2014</p>
