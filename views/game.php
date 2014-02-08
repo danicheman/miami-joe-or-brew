d04a2494f42a1ba45226210eedc9059243b9e03f
Todd Wirth <madmx5@phyberoptiks.net>
2014-02-07 22:42:50 -0500

    changes to gameboard

--- views/game.php
+++ views/game.php
@@ -1,4 +1,51 @@
-<div>
-    <img class="drink" src="../images/beer-icon.gif">
-    <img class="drink" src="../images/coffee-icon.png">
-</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Miami Brew vs. Brew</title>
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../dist/css/custom.css">    
+
+    <!-- Just for debugging purposes. Don't actually copy this line! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body id="gameBoard">
+
+    <header>
+        <h1 id="question_text">________ Can Island</h1>
+        <h2 id="question_hint">Are they serving cold ones or cafe con leche?</h2>
+    </header>
+
+    <article class="versus-row">
+        vs.
+    </article>
+
+    <article class="row">
+        <section id="beer" class="col-md-6">
+            <div class="row">
+                <a href="#"><img class="col-md-6 col-md-offset-3" src="../images/beer-icon.gif"></a>
+            </div>
+        </section>
+
+        <section id="cafe" class="col-md-6">
+            <div class="row">
+                <a href="#"><img class="col-md-8 col-md-offset-1" src="../images/coffee-icon.png"></a>
+            </div>
+        </section>
+    </article>
+
+</body>
+</html>
