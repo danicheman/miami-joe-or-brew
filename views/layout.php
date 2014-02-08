5bf94ac286426e830b0332306692822b7d055557
Nick O <danicheman@yahoo.com>
2014-02-07 21:31:41 -0500

    asdfasfd

--- views/layout.php
+++ views/layout.php
@@ -40,9 +40,9 @@
       </div>
 
       <div class="jumbotron">
-        <h1>Jumbotron heading</h1>
-        <p class="lead">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-        <p><a class="btn btn-lg btn-success" href="#" role="button">Sign up today</a></p>
+        <h1>Brew vs. Brew</h1>
+        <p class="lead">Welcome to Brew vs. Brew!  Miami's favorite online trivia game.  Don't let the ZOMBIE eat the PHPELLEPHANT!</p>
+        <p><button class="btn btn-lg btn-success" href="#" role="button">Start now!</button></p>
       </div>
 
       <?php
