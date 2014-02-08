9db7087a325b1418f1c2a302824752b76c990873
Nick O <danicheman@yahoo.com>
2014-02-07 23:22:33 -0500

    lkajsdf

--- index.php
+++ index.php
@@ -6,6 +6,8 @@ if(!empty($_GET['page']) && in_array($_GET['page'],['start','win', 'lose','game'
     
     if($_GET['page'])include('/views/layout.php');    
     include('views/'.$_GET['page'].'.php');
+}else {
+    include('views/game.php');
 }
 
 ?>
