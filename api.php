b33e23826d96dba0ff9690ebece9844cd6e80ae6
K-Styles <K-Styles@K-Styless-MacBook-Pro-2.local>
2014-02-07 22:39:49 -0500

    update api.php

--- api.php
+++ api.php
@@ -1,21 +1,21 @@
 <?php
 
-//include_once("game_manager.class.php");
 include_once("foursquare.php");
+include_once("game_manager.class.php");
 
-//$game_manager = new game_manager();
-echo "<pre>";
-print_r($_POST);
-if($start){
-	
-	echo json_encode($_SESSION["game"]);
+$type = $_POST["type"];
+$answer = $_POST["answer"];
+$question_index = $_POST["question_index"];
+if($type != ""){
+	$game_manager = new game_manager();
+	if($type == "start"){
+		echo json_encode($_SESSION["game"]);
+	}
+	elseif($type == "answer" && $answer != "" && $question_index != ""){
+		$game_manager->check_answer($answer,$question_index);
+		echo json_encode($_SESSION["game"]);
+	}
 }
-else{
-	
-}
 
-
-
-
-
+exit;
 ?>
